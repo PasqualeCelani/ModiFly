e88df0bb60edf43c7bf7d0bda5df14dcd5ef27bd
PasqualeCelani <thespeedkiller01@gmail.com>
2025-07-18 14:20:06 +0300

    Accellefation/Decelleration and speed limit with non costant speed have been implemented

--- scripts/drone.gd
+++ scripts/drone.gd
@@ -1,43 +1,59 @@
 extends CharacterBody3D
 
-@export var speed = 20
-@export var vertical_speed: float = 2.0
-@export var rotation_speed: float = 30.0 
+@export var max_speed: float = 50.0
+@export var vertical_speed: float = 5.0
+@export var rotation_speed: float = 30.0
 
-var target_velocity = Vector3.ZERO
+@export var acceleration: float = 10.0
+@export var deceleration: float = 8.0
+@export var vertical_acceleration: float = 5.0
+@export var vertical_deceleration: float = 5.0
+@export var speed_ramp_rate: float = 10.0  # How fast speed increases/decreases
+
+var current_speed: float = 0.0  # Incremental horizontal speed
+var current_velocity = Vector3.ZERO
+var target_direction = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
 	var gesture_node = get_node("/root/MainScene/GestureClient")
 	var command = gesture_node.current_command
+
+	target_direction = Vector3.ZERO
 	
 	if command["left"] == "right":
-		direction.x += 1
-	if command["left"]  == "left":
-		direction.x -= 1
-	if command["left"]  == "backward":
-		direction.z += 1
-	if command["left"]  == "forward":
-		direction.z -= 1
-		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		direction = global_transform.basis * direction
-		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+		target_direction.x += 1
+	if command["left"] == "left":
+		target_direction.x -= 1
+	if command["left"] == "backward":
+		target_direction.z += 1
+	if command["left"] == "forward":
+		target_direction.z -= 1
+
+	var input_active = target_direction != Vector3.ZERO
 	
-	if command["left"]  == "up":
-		target_velocity.y = vertical_speed
-	elif  command["left"]  == "down":
-		target_velocity.y = -vertical_speed
+	if input_active:
+		target_direction = global_transform.basis * target_direction.normalized()
+	
+	if input_active:
+		current_speed = move_toward(current_speed, max_speed, speed_ramp_rate * delta)
 	else:
-		target_velocity.y = 0
-		
-	if command["right"]  == "rotate_right":
+		current_speed = move_toward(current_speed, 0.0, speed_ramp_rate * delta)
+	
+	var desired_velocity = target_direction * current_speed
+	current_velocity.x = move_toward(current_velocity.x, desired_velocity.x, acceleration * delta)
+	current_velocity.z = move_toward(current_velocity.z, desired_velocity.z, acceleration * delta)
+
+	if command["left"] == "up":
+		current_velocity.y = move_toward(current_velocity.y, vertical_speed, vertical_acceleration * delta)
+	elif command["left"] == "down":
+		current_velocity.y = move_toward(current_velocity.y, -vertical_speed, vertical_acceleration * delta)
+	else:
+		current_velocity.y = move_toward(current_velocity.y, 0.0, vertical_deceleration * delta)
+	
+	if command["right"] == "rotate_right":
 		rotation_degrees.y -= rotation_speed * delta
-	elif command["right"]  == "rotate_left":
+	elif command["right"] == "rotate_left":
 		rotation_degrees.y += rotation_speed * delta
 	
-	velocity = target_velocity
+	velocity = current_velocity
 	move_and_slide()
