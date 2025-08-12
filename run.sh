b523508d3b9adcb0db04b21241d6c73b983f7301
PasqualeCelani <thespeedkiller01@gmail.com>
2025-08-12 22:47:07 +0300

    fix on run script (auto killing python server on start)

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd ../server
+cd ./server
 source ./venv/bin/activate
 
 python main.py &
 PYTHON_PID=$!
-echo "Python server has start!"
+echo "Python server has started with PID: $PYTHON_PID"
 
 open /Applications/Modifly.app
-echo "Modifly has open!"
+echo "Modifly has been opened."
 
 # Loop until the Modifly app is no longer running
-echo "Monitoring Modifly. The Python process will be killed when Modifly closes"
-while pgrep -f -x "Modifly" > /dev/null; do
+echo "Monitoring Modifly. The Python process will be killed when Modifly closes."
+while pgrep -i -x "Modifly" > /dev/null; do
   sleep 5
 done
 
